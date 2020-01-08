76b8f4011816052dd134165d9b85808ee831bb9d
Georgy <georgyostr@gmail.com>
2020-01-09 00:52:49 +0900

    config bits set

--- configuration_bits.c
+++ configuration_bits.c
@@ -8,26 +8,22 @@
     #include <htc.h>        /* HiTech General Include File */
 #endif
 
-/******************************************************************************/
-/* Configuration Bits                                                         */
-/*                                                                            */
-/* Refer to your Hi-Tech User Manual in the PICC installation directory       */
-/* /doc folder for more information on filling in configuration bits.         */
-/* In addition, configuration bit mnemonics can be found in your              */
-/* PICC\version\include\<processor name>.h file for your device.  The XC8     */
-/* compiler contains documentation on the configuration bit macros within     */
-/* the compiler installation /docs folder in a file called                    */
-/* pic18_chipinfo.html.                                                       */
-/*                                                                            */
-/* For additional information about what the hardware configurations mean in  */
-/* terms of device operation, refer to the device datasheet.                  */
-/*                                                                            */
-/* A feature of MPLAB X is the 'Generate Source Code to Output' utility in    */
-/* the Configuration Bits window.  Under Window > PIC Memory Views >          */
-/* Configuration Bits, a user controllable configuration bits window is       */
-/* available to Generate Configuration Bits source code which the user can    */
-/* paste into this project.                                                   */
-/*                                                                            */
-/******************************************************************************/
 
-/* TODO Fill in your config bits here.  Use the configuration bits generator/ */
+// PIC16F628A Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+#pragma config FOSC = INTOSCIO  // Oscillator Selection bits (INTOSC oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is digital input, MCLR internally tied to VDD)
+#pragma config BOREN = OFF      // Brown-out Detect Enable bit (BOD disabled)
+#pragma config LVP = ON         // Low-Voltage Programming Enable bit (RB4/PGM pin has PGM function, low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EE Memory Code Protection bit (Data memory code protection off)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
